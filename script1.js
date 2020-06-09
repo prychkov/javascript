e37b01a91385d6f7528a40e9b04785e5aeb8b44d
Rychkov Pavel <rychkovpavel@yandex.kz>
2020-06-09 08:28:12 +0600

    First commit

---
+++ script1.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/* a = 5;
+console.log(a); выйдет ошибка в строгой (use strict) версии JS и нет без
+(use strict) */ 
+
+// "Comment me"
+
+/* "And" 
+    "ME!" */
+
+
+var leftBorderWidth = 1;
+let second = 2;
+
+
+const pi = 3.14;
+console.log(second);
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*4);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plun.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[0]);
+
+/* alert ("Hello world"); */
+/* let answer = confirm ("Are you here?");
+console.log(answer); */
+
+/* let answer = +prompt("Вам есть 18 лет?", "Да");
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+console.log(4 + +" - object"); */
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
